de68e16631f37705e353bd7ff110c2c38f2150c0
root <deeprajp@google.com>
2024-07-26 13:46:55 +0000

    fixing import for unit test

--- mmv1/third_party/terraform/services/iap/resource_iap_settings_test.go
+++ mmv1/third_party/terraform/services/iap/resource_iap_settings_test.go
@@ -3,7 +3,7 @@ package iap_test
 import (
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 )
@@ -18,7 +18,6 @@ func TestAccIapSettings_update(t *testing.T) {
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckIapSettingsDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccIapSettings_basic(context),
@@ -36,108 +35,91 @@ func TestAccIapSettings_update(t *testing.T) {
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
-						{
-				Config: testAccIapSettings_delete(context),
-			},
-			{
-				ResourceName:      "google_iap_settings.default",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
 		},
 	})
 }
 
-
-
 func testAccIapSettings_basic(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_compute_region_backend_service" "default" {
-  name                            = "backend-service"
-  region                          = "us-central1"
-  health_checks                   = [google_compute_health_check.default.id]
-  connection_draining_timeout_sec = 10
-  session_affinity                = "CLIENT_IP"
-}
+	
+	resource "google_compute_region_backend_service" "default" {
+  	  name                            = "tf-test-iap-settings-tf%{random_suffix}"
+  	  region                          = "us-central1"
+  	  health_checks                   = [google_compute_health_check.default.id]
+  	  connection_draining_timeout_sec = 10
+  	  session_affinity                = "CLIENT_IP"
+	}
 
-resource "google_compute_health_check" "default" {
-  name               = "rbs-health-check"
-  check_interval_sec = 1
-  timeout_sec        = 1
+	resource "google_compute_health_check" "default" {
+  	  name               = "tf-test-iap-bs-health-check%{random_suffix}"
+  	  check_interval_sec = 1
+  	  timeout_sec        = 1
+	  tcp_health_check {
+  	    port = "80"
+	  }
+	}
 
-  tcp_health_check {
-    port = "80"
-  }
-}
-
-resource "google_iap_settings" "default" {
-  name = "projects/test_project_id/iap_web/compute-us-central1/services/${google_compute_region_backend_service.default.name}"
-  access_settings {
-    identity_sources = ["IDENTITY_SOURCE_UNSPECIFIED"]
-    cors_settings {
-      allow_http_options = true
-    }
-    reauth_settings {
-      method = "LOGIN"
-      max_age = "405s"
-      policy_type = "MINIMUM"
-    }
-    allowed_domains_settings {
-      domains = ["xyz.org","abc.in"]
-      enable = true
-    }
-  }
-  application_settings {
-    csm_settings {
-      rctoken_aud = "audience"
-    }
-    access_denied_page_settings {
-      access_denied_page_uri = "access-denied-uri"
-      generate_troubleshooting_uri = true
-      remediation_token_generation_enabled = true
-    }
-  }
-}
+	resource "google_iap_settings" "default" {
+	  name = "projects/397988910753/iap_web/compute-us-central1/services/${google_compute_region_backend_service.default.id}"
+	  access_settings {
+	    identity_sources = ["IDENTITY_SOURCE_UNSPECIFIED"]
+	    cors_settings {
+	      allow_http_options = true
+	    }
+	    reauth_settings {
+	      method = "LOGIN"
+	      max_age = "405s"
+	      policy_type = "MINIMUM"
+	    }
+	    allowed_domains_settings {
+	      domains = ["xyz.org", "abc.in"]
+	      enable  = true
+	    }
+	  }
+	  application_settings {
+	    csm_settings {
+	      rctoken_aud = "audience"
+	    } 
+	    access_denied_page_settings {
+	      access_denied_page_uri = "access-denied-uri"
+	      generate_troubleshooting_uri = true
+	      remediation_token_generation_enabled = true
+	    }   
+	  }
+	}
 `, context)
 }
 
 func testAccIapSettings_update(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-
-resource "google_iap_settings" "default" {
-  name = "projects/test_project_id/iap_web/compute-us-central1/services/${google_compute_region_backend_service.default.name}"
-  access_settings {
-    gcip_settings {
-      login_page_uri = "https://test.com/?apiKey=abcd_efgh"
-      tenant_ids = ["tenant1","tenant2"]
-    }
-    oauth_settings {
-      login_hint = "test"
-      programmatic_clients = ["client_ids"]
-    }
-    workforce_identity_settings {
-      oauth2 {
-        client_id = "test_id"
-        client_secret = "test"
-      }
-      workforce_pools = ["wifpool"]
-    }
-  }
-  application_settings {
-    attribute_propagation_settings {
-      enable = true
-      expression = "test"
-      output_credentials = ["HEADER"]
-    }
-    cookie_domain = "org"
-  }
-}
+	
+	resource "google_iap_settings" "default" {
+	  name = "projects/test_project_id/iap_web/compute-us-central1/services/${google_compute_region_backend_service.default.name}"
+	  access_settings {
+	    gcip_settings {
+	      login_page_uri = "https://test.com/?apiKey=abcd_efgh"
+	      tenant_ids = ["tenant1","tenant2"]
+	    }
+	    oauth_settings {
+	      login_hint = "test"
+	      programmatic_clients = ["client_ids"]
+	    }
+	    workforce_identity_settings {
+	      oauth2 {
+	        client_id = "test_id"
+	        client_secret = "test"
+	      }
+	      workforce_pools = ["wifpool"]
+	    }
+	  }
+	  application_settings {
+	    attribute_propagation_settings {
+	      enable = true
+	      expression = "test"
+	      output_credentials = ["HEADER"]
+	    }
+	    cookie_domain = "org"
+	  }
+	}
 `, context)
 }
-
-func testAccIapSettings_delete(context map[string]interface{}) string {
-	return acctest.Nprintf(`
-
-`, context)
-}
-
