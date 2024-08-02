ed38f1218fdef41803d05dc211ce0c31929fc5b6
root <deeprajp@google.com>
2024-08-02 11:42:53 +0000

    fixing client secret issue

--- mmv1/third_party/terraform/services/iap/resource_iap_settings_test.go
+++ mmv1/third_party/terraform/services/iap/resource_iap_settings_test.go
@@ -27,6 +27,7 @@ func TestAccIapSettings_update(t *testing.T) {
 				ResourceName:      "google_iap_settings.iap_settings",
 				ImportState:       true,
 				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{"access_settings.0.workforce_identity_settings.0.oauth2.0.client_secret"},
 			},
 		},
 	})
